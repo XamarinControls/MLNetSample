171c93aae7fc209bf2b0eaf92657dc1e17c24e5b
Bryan Anthony Garcia <bryan@xam-consulting.com>
2018-12-12 19:54:35 +0800

    Add project files.

---
+++ SampleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Data;
+using Microsoft.ML.Legacy.Models;
+using Microsoft.ML.Legacy.Trainers;
+using Microsoft.ML.Legacy.Transforms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pipeline = new LearningPipeline();
+
+            pipeline.Add(new TextLoader("SalaryData.csv").CreateFrom<SalaryData>(useHeader: true, separator: ','));
+            pipeline.Add(new ColumnConcatenator("Features", "YearsExperience"));
+            pipeline.Add(new GeneralizedAdditiveModelRegressor());
+
+            var model = pipeline.Train<SalaryData, SalaryPrediction>();
+
+            var testData = new TextLoader("SalaryData-test.csv").CreateFrom<SalaryData>(useHeader: true, separator: ',');
+
+            var evaluator = new RegressionEvaluator();
+            var metrics = evaluator.Evaluate(model, testData);
+
+            Console.WriteLine("RMS - " + metrics.Rms);
+            Console.WriteLine("R^2 - " + metrics.RSquared);
+
+            float yearsExperience = 7;
+
+            var prediction = model.Predict(new SalaryData() { YearsExperience = yearsExperience });
+
+            Console.WriteLine("Salary prediction for employee with " + yearsExperience + " years experience is " + prediction.PredictedSalary);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SampleApp/SalaryData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    public class SalaryData
+    {
+        [Column("0")]
+        public float YearsExperience;
+
+        [Column("1", name: "Label")]
+        public float Salary;
+    }
+}
